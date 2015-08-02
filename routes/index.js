d80ea8d8d7b5dde5d5b0e8dafc870e733553ec2c
Daniel Diaz <diazjfdaniel@gmail.com>
2015-08-01 23:23:14 -0700

    Map

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+var googleApiKey = process.env.GOOGLE_API_KEY;
 module.exports = function(router) {
     router.get('/', function (req, res) {
         return res.render('index');
@@ -6,6 +7,6 @@ module.exports = function(router) {
         return res.render('activeStudents');
     });
     router.get('/map', function(req, res) {
-        return res.render('map');
+        return res.render('map', {key: googleApiKey});
     })
 }
