da1d9b710203c658ce48288cc17071286417b878
Brian De Anda <briandeanda2@gmail.com>
2015-08-02 17:14:35 -0700

    added event Schema, added ids to schemas, styling of views

---
+++ models/Event.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema=mongoose.Schema;
+var autoIncrement = require('mongoose-auto-increment');
+
+var eventsSchema = new Schema({
+    id: Integer,
+    name: String,
+    date: Date,
+    lat: Double,
+    long: Double,
+    location: String,
+    summary: String,
+    members: [],
+    isHackthon: Boolean,
+    isInternship: Boolean,
+    isConference: Boolean,
+    isFulltime: Boolean
+});
+
+eventsSchema.plugin(autoIncrement.plugin, { model: 'Events', startAt: 1 });
+var Events = mongoose.model('Events', eventsSchema);
+module.exports = Events;
