7dcc222143504450d8cd3e1a345bb9ad98924d3f
Daniel Diaz <diazjfdaniel@gmail.com>
2015-08-02 18:59:22 -0700

    Events routing

---
+++ routes/events.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+
+var EventsSchema = mongoose.model('Events');
+module.exports = function(router) {
+
+    //push product details into the database
+
+    router.get('/events', function (req, res){
+        var EventsSchema = require('../models/Event');
+
+        return EventsSchema.find({}).lean().exec(function (err, events) {
+            if (!err) {
+                var ob = events[0];
+
+                return res.send(ob);
+            } else {
+                return console.log(err);
+            }
+        });
+    });
+}
