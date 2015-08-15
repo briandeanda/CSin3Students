ceedd47f5c8f1131b6a32df025907f2cf0619eae
Daniel Diaz <diazjfdaniel@gmail.com>
2015-08-14 17:51:13 -0700

    Updated shell script

--- setup.sh
+++ setup.sh
@@ -2,17 +2,24 @@
 
 setupMac() {
   which -s brew;
-  if [[ $? != 0]]  then
-    echo "Nooo"
+  if [[ $? != 0 ]] ; then
+    echo "Install brew"
   else
-    brew update
+	echo "Updating brew...";
+	brew update -v
+	echo "Running brew doctor";
+	brew doctor -v
+	echo "Installing node...";
+	brew install node
+	echo "Installing mongodb...";
+	brew install mongodb
   fi
 }
         
 
 if [ "$(uname)" == "Darwin" ]; then
     echo 'Hello Mac User ;)';
-    setupMac();
+    setupMac;
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     echo 'Hello Linux user :)';
 fi
